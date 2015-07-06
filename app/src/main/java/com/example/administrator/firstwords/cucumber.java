a38067a663eb12b5216b49b244194a1bc26528d3
adamfilipczyk <adamfilipczyk@gmail.com>
2015-07-06 16:23:10 +0100

    First Words upload

--- app/src/main/java/com/example/administrator/firstwords/cucumber.java
+++ app/src/main/java/com/example/administrator/firstwords/cucumber.java
@@ -1,38 +1,229 @@
 package com.example.administrator.firstwords;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.media.SoundPool;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.DragEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import java.util.Random;
 
 
-public class cucumber extends ActionBarActivity {
+public class cucumber extends Activity implements View.OnTouchListener, View.OnDragListener {
 
+    SoundPool sound, click;
+    int soundID;
+    MediaPlayer mMediaPlayer, correct1, correct2, correct3, incorr1, incorr2, incorr3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cucumber);
+
+        //player
+        mMediaPlayer = MediaPlayer.create(this, R.raw.cucumber);
+        mMediaPlayer.start();
+
+
+        //object for the speaker sound
+        sound = new SoundPool(1, AudioManager.STREAM_MUSIC, 0);
+        soundID = sound.load(this, R.raw.cucumber, 1);
+
+        //object for the check sound
+        click = new SoundPool(1, AudioManager.STREAM_MUSIC, 0);
+        soundID = click.load(this, R.raw.click, 1);
+
+
+        //letters
+        findViewById(R.id.cucumber_c).setOnTouchListener(this);
+        findViewById(R.id.cucumber_u).setOnTouchListener(this);
+        findViewById(R.id.cucumber_c2).setOnTouchListener(this);
+        findViewById(R.id.cucumber_u).setOnTouchListener(this);
+        findViewById(R.id.cucumber_m).setOnTouchListener(this);
+        findViewById(R.id.cucumber_b).setOnTouchListener(this);
+        findViewById(R.id.cucumber_e).setOnTouchListener(this);
+        findViewById(R.id.cucumber_r).setOnTouchListener(this);
+
+        //bottom containers drag listener
+        findViewById(R.id.bottom_c).setOnDragListener(this);
+        findViewById(R.id.bottom_u).setOnDragListener(this);
+        findViewById(R.id.bottom_c2).setOnDragListener(this);
+        findViewById(R.id.bottom_u2).setOnDragListener(this);
+        findViewById(R.id.bottom_m).setOnDragListener(this);
+        findViewById(R.id.bottom_b).setOnDragListener(this);
+        findViewById(R.id.bottom_e).setOnDragListener(this);
+        findViewById(R.id.bottom_r).setOnDragListener(this);
+        //top container drag listener
+        findViewById(R.id.top_c).setOnDragListener(this);
     }
 
+    //method to play the sound of the picture displayed on the screen
+    public void play(View view) {
+        // sound.play(soundID, 1, 1, 1, 0, 1);
+    }
+
+    //method to play the sound of the picture displayed on the screen
+    public void back(View view) {
+        click.play(soundID, 1, 1, 1, 0, 1);
+        finish();
+        Intent home = new Intent(this, home.class);
+        startActivity(home);
+    }
+
+
+    //method for dragging object
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_cucumber, menu);
+    public boolean onDrag(View v, DragEvent event) {
+        if (event.getAction() == DragEvent.ACTION_DROP){
+            View view = (View) event.getLocalState();
+            ViewGroup group = (ViewGroup) view.getParent();
+            group.removeView(view);
+            LinearLayout target = (LinearLayout) v;
+            target.addView(view);
+            view.setVisibility(View.VISIBLE);
+        }
         return true;
     }
 
+    //method to set object moving
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+    public boolean onTouch(View v, MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN){
+            View.DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(v);
+            v.startDrag(null, shadowBuilder, v, 0);
             return true;
         }
+        else {
+            return false;
+        }
+    }
 
-        return super.onOptionsItemSelected(item);
+
+    public void check (View view) {
+
+        click.play(soundID, 1, 1, 1, 0, 1);
+
+        LinearLayout b_m = (LinearLayout)findViewById(R.id.bottom_m);
+        ImageView m = (ImageView) b_m.findViewById(R.id.cucumber_m);
+
+        LinearLayout b_e = (LinearLayout)findViewById(R.id.bottom_e);
+        ImageView e = (ImageView) b_e.findViewById(R.id.cucumber_e);
+
+        LinearLayout b_r = (LinearLayout)findViewById(R.id.bottom_r);
+        ImageView r = (ImageView) b_r.findViewById(R.id.cucumber_r);
+
+        LinearLayout b_b = (LinearLayout)findViewById(R.id.bottom_b);
+        ImageView b = (ImageView) b_b.findViewById(R.id.cucumber_b);
+
+        //c----------------------------------------------------------
+
+        LinearLayout b_c1 = (LinearLayout)findViewById(R.id.bottom_c);
+        ImageView c1 = (ImageView) b_c1.findViewById(R.id.cucumber_c);
+
+        LinearLayout b_c2 = (LinearLayout)findViewById(R.id.bottom_c2);
+        ImageView c2 = (ImageView) b_c2.findViewById(R.id.cucumber_c2);
+
+        LinearLayout b_c3 = (LinearLayout)findViewById(R.id.bottom_c);
+        ImageView c3 = (ImageView) b_c3.findViewById(R.id.cucumber_c2);
+
+        LinearLayout b_c4 = (LinearLayout)findViewById(R.id.bottom_c2);
+        ImageView c4 = (ImageView) b_c4.findViewById(R.id.cucumber_c);
+
+        //u----------------------------------------------------------
+
+        LinearLayout b_u1 = (LinearLayout)findViewById(R.id.bottom_u);
+        ImageView u1 = (ImageView) b_u1.findViewById(R.id.cucumber_u);
+
+        LinearLayout b_u2 = (LinearLayout)findViewById(R.id.bottom_u2);
+        ImageView u2 = (ImageView) b_u2.findViewById(R.id.cucumber_u2);
+
+        LinearLayout b_u3 = (LinearLayout)findViewById(R.id.bottom_u);
+        ImageView u3 = (ImageView) b_u3.findViewById(R.id.cucumber_u2);
+
+        LinearLayout b_u4 = (LinearLayout)findViewById(R.id.bottom_u2);
+        ImageView u4 = (ImageView) b_u4.findViewById(R.id.cucumber_u);
+
+
+        correct1 = MediaPlayer.create(this, R.raw.welldone);
+        correct2 = MediaPlayer.create(this, R.raw.congrats);
+        correct3 = MediaPlayer.create(this, R.raw.didit);
+
+        incorr1 = MediaPlayer.create(this, R.raw.rusure);
+        incorr2 = MediaPlayer.create(this, R.raw.incorrect);
+        incorr3 = MediaPlayer.create(this, R.raw.tryagain);
+
+        if ((m!= null) && (b!= null) && (e!=null) && (r!=null) && ((c1!=null && c2!=null) || (c3!=null && c4!=null)) && ((u1!=null && u2!=null) || (u3!=null && u4!=null))) {
+
+            Random generate = new Random();
+            int eventNumber = generate.nextInt(3)+1;
+
+            switch (eventNumber){
+                case 1:
+                    correct1.start();
+                    correct1.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                        public void onCompletion(MediaPlayer mp) {
+                            mp.release();
+                            finish();
+                            Intent oni = new Intent(getApplicationContext(), onion.class);
+                            startActivity(oni);
+                        }
+                    });
+                    break;
+                case 2:
+                    correct2.start();
+                    correct2.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                        public void onCompletion(MediaPlayer mp) {
+                            mp.release();
+                            finish();
+                            Intent oni = new Intent(getApplicationContext(), onion.class);
+                            startActivity(oni);
+                        }
+                    });
+                    break;
+                case 3:
+                    correct3.start();
+                    correct3.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                        public void onCompletion(MediaPlayer mp) {
+                            mp.release();
+                            finish();
+                            Intent oni = new Intent(getApplicationContext(), onion.class);
+                            startActivity(oni);
+                        }
+                    });
+                    break;
+                default:
+                    //do nothing
+                    break;
+            }
+        }
+        else {
+
+            Random generate = new Random();
+            int eventNumber = generate.nextInt(3)+1;
+
+            switch (eventNumber){
+                case 1:
+                    incorr1.start();
+                    break;
+                case 2:
+                    incorr2.start();
+                    break;
+                case 3:
+                    incorr3.start();
+                    break;
+                default:
+                    //do nothing
+                    break;
+            }
+        }
     }
 }
