63d6b60c9ab2242c9cf566a9c60509407532ef4c
adamfilipczyk <adamfilipczyk@gmail.com>
2015-07-06 15:49:15 +0100

    First Words upload

--- app/src/main/java/com/example/administrator/firstwords/carrot.java
+++ app/src/main/java/com/example/administrator/firstwords/carrot.java
@@ -1,38 +1,209 @@
 package com.example.administrator.firstwords;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.media.SoundPool;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.DragEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import java.util.Random;
 
 
-public class carrot extends ActionBarActivity {
+public class carrot extends Activity implements View.OnTouchListener, View.OnDragListener  {
+
+        SoundPool sound, click;
+        int soundID;
+        MediaPlayer mMediaPlayer, correct1, correct2, correct3, incorr1, incorr2, incorr3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_carrot);
+
+        //player
+        mMediaPlayer = MediaPlayer.create(this, R.raw.carrot);
+        mMediaPlayer.start();
+
+
+        //object for the speaker sound
+        sound = new SoundPool(1, AudioManager.STREAM_MUSIC, 0);
+        soundID = sound.load(this, R.raw.carrot, 1);
+
+        //object for the check sound
+        click = new SoundPool(1, AudioManager.STREAM_MUSIC, 0);
+        soundID = click.load(this, R.raw.click, 1);
+
+
+        //letters
+        findViewById(R.id.carrot_c).setOnTouchListener(this);
+        findViewById(R.id.carrot_a).setOnTouchListener(this);
+        findViewById(R.id.carrot_r).setOnTouchListener(this);
+        findViewById(R.id.carrot_r2).setOnTouchListener(this);
+        findViewById(R.id.carrot_o).setOnTouchListener(this);
+        findViewById(R.id.carrot_t).setOnTouchListener(this);
+
+        //bottom containers drag listener
+        findViewById(R.id.bottom_c).setOnDragListener(this);
+        findViewById(R.id.bottom_a).setOnDragListener(this);
+        findViewById(R.id.bottom_r).setOnDragListener(this);
+        findViewById(R.id.bottom_r2).setOnDragListener(this);
+        findViewById(R.id.bottom_o).setOnDragListener(this);
+        findViewById(R.id.bottom_t).setOnDragListener(this);
+        //top container drag listener
+        findViewById(R.id.top_c).setOnDragListener(this);
     }
 
+    //method to play the sound of the picture displayed on the screen
+    public void play(View view) {
+        // sound.play(soundID, 1, 1, 1, 0, 1);
+    }
+
+    //method to play the sound of the picture displayed on the screen
+    public void back(View view) {
+        click.play(soundID, 1, 1, 1, 0, 1);
+        finish();
+        Intent home = new Intent(this, home.class);
+        startActivity(home);
+    }
+
+
+    //method for dragging object
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_carrot, menu);
+    public boolean onDrag(View v, DragEvent event) {
+        if (event.getAction() == DragEvent.ACTION_DROP){
+            View view = (View) event.getLocalState();
+            ViewGroup group = (ViewGroup) view.getParent();
+            group.removeView(view);
+            LinearLayout target = (LinearLayout) v;
+            target.addView(view);
+            view.setVisibility(View.VISIBLE);
+        }
         return true;
     }
 
+    //method to set object moving
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+    public boolean onTouch(View v, MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN){
+            View.DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(v);
+            v.startDrag(null, shadowBuilder, v, 0);
             return true;
         }
+        else {
+            return false;
+        }
+    }
 
-        return super.onOptionsItemSelected(item);
+
+    public void check (View view) {
+
+        click.play(soundID, 1, 1, 1, 0, 1);
+
+        LinearLayout b_c = (LinearLayout)findViewById(R.id.bottom_c);
+        ImageView c = (ImageView) b_c.findViewById(R.id.carrot_c);
+
+        LinearLayout b_o = (LinearLayout)findViewById(R.id.bottom_o);
+        ImageView o = (ImageView) b_o.findViewById(R.id.carrot_o);
+
+        LinearLayout b_t = (LinearLayout)findViewById(R.id.bottom_t);
+        ImageView t = (ImageView) b_t.findViewById(R.id.carrot_t);
+
+        //r----------------------------------------------------------
+
+        LinearLayout b_r1 = (LinearLayout)findViewById(R.id.bottom_r);
+        ImageView r1 = (ImageView) b_r1.findViewById(R.id.carrot_r);
+
+        LinearLayout b_r2 = (LinearLayout)findViewById(R.id.bottom_r2);
+        ImageView r2 = (ImageView) b_r2.findViewById(R.id.carrot_r2);
+
+        LinearLayout b_r3 = (LinearLayout)findViewById(R.id.bottom_r);
+        ImageView r3 = (ImageView) b_r3.findViewById(R.id.carrot_r2);
+
+        LinearLayout b_r4 = (LinearLayout)findViewById(R.id.bottom_r2);
+        ImageView r4 = (ImageView) b_r4.findViewById(R.id.carrot_r);
+
+
+        correct1 = MediaPlayer.create(this, R.raw.welldone);
+        correct2 = MediaPlayer.create(this, R.raw.congrats);
+        correct3 = MediaPlayer.create(this, R.raw.didit);
+
+        incorr1 = MediaPlayer.create(this, R.raw.rusure);
+        incorr2 = MediaPlayer.create(this, R.raw.incorrect);
+        incorr3 = MediaPlayer.create(this, R.raw.tryagain);
+
+        if ((c!= null) && (o!= null) && (t!=null) && ((r1!=null && r2!=null) || (r3!=null && r4!=null))) {
+
+            Random generate = new Random();
+            int eventNumber = generate.nextInt(3)+1;
+
+            switch (eventNumber){
+                case 1:
+                    correct1.start();
+                    correct1.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                        public void onCompletion(MediaPlayer mp) {
+                            mp.release();
+                            finish();
+                            Intent cuc = new Intent(getApplicationContext(), cucumber.class);
+                            startActivity(cuc);
+                        }
+                    });
+                    break;
+                case 2:
+                    correct2.start();
+                    correct2.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                        public void onCompletion(MediaPlayer mp) {
+                            mp.release();
+                            finish();
+                            Intent cuc = new Intent(getApplicationContext(), cucumber.class);
+                            startActivity(cuc);
+                        }
+                    });
+                    break;
+                case 3:
+                    correct3.start();
+                    correct3.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                        public void onCompletion(MediaPlayer mp) {
+                            mp.release();
+                            finish();
+                            Intent cuc = new Intent(getApplicationContext(), cucumber.class);
+                            startActivity(cuc);
+                        }
+                    });
+                    break;
+                default:
+                    //do nothing
+                    break;
+            }
+        }
+        else {
+
+            Random generate = new Random();
+            int eventNumber = generate.nextInt(3)+1;
+
+            switch (eventNumber){
+                case 1:
+                    incorr1.start();
+                    break;
+                case 2:
+                    incorr2.start();
+                    break;
+                case 3:
+                    incorr3.start();
+                    break;
+                default:
+                    //do nothing
+                    break;
+            }
+        }
     }
 }
